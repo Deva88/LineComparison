4ea4cf712f53454d9c28ce9dc2509e4e29860fea
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-24 22:23:37 +0530

    Added: Line based on a point consisting of (x,y)

--- src/com/linecomparison/LineComparison.java
+++ src/com/linecomparison/LineComparison.java
@@ -3,7 +3,19 @@ package com.linecomparison;
 public class LineComparison {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To Line Comparison");
+		// Variables
+		int x1, x2, y1, y2;
+		int line1;
+		// Taking Input
+		x1 = 2;
+		y1 = 2;
+		x2 = 4;
+		y2 = 4;
+
+		line1 = (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+		// print Length of line
+		System.out.println("Length of Line is " + line1);
 
 	}
 

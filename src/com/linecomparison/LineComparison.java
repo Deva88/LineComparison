e15c760ec6793c5e034fc22161c1fa396f5354c8
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-25 13:00:21 +0530

    Line and point as well as equals and compareTo method.

--- src/com/linecomparison/LineComparison.java
+++ src/com/linecomparison/LineComparison.java
@@ -2,40 +2,70 @@ package com.linecomparison;
 
 public class LineComparison {
 
-	public static void main(String[] args) {
-		// Variables
+	// create function for equal()
+	public static void equal() {
+		// member Variables
+		int x1, x2, y1, y2, x3, y3; // line coordinates
+		int line1, line2;
+		x1 = 1; // initialize value
+		y1 = 1;
+		x2 = 0;
+		y2 = 5;
+		x3 = 9;
+		y3 = 0;
+
+		// calculate length of a line
+		line1 = (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		line2 = (int) Math.sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
+		// cover the double into string and store the value length1
+		String s2 = String.valueOf(line2);
+		String s1 = String.valueOf(line1);
+
+		// checking lines are equal or not
+		System.out.println(s1.equals(s2));
+	}
+
+	//// create function for CompareTo()
+	public static void compare() {
+
+		// member Variables
 		int x1, x2, y1, y2, x3, y3;
 		int line1, line2;
-
-		// initialize value
-		x1 = 2;
+		x1 = 1;// initialize value
 		y1 = 2;
-		x2 = 4;
+		x2 = 3;
 		y2 = 4;
-		x3 = 5;
-		y3 = 1;
-		// computation of length of line
+		x3 = 8;
+		y3 = 0;
+		// calculate of length of line
 		line1 = (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 		line2 = (int) Math.sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
 
 		// print line 1 and line 2 value
-		System.out.println("Lenth of Line 1 value =" + line1);
-		System.out.println("Length od Line 2 value =" + line2);
+		System.out.println("Line1  Length = " + line1);
+		System.out.println("Line2  Length = " + line2);
 
 		// creating a object of integer class
-		Integer length1 = new Integer(line1);
-		Integer length2 = new Integer(line2);
+		Integer a = new Integer(line1);
+		Integer b = new Integer(line2);
 
 		// comparing the lines length using compareTo() method
-		int compare = length1.compareTo(length2);
+		int compare = a.compareTo(b);
 
+		// checking lines are equal,greater or smaller than one another
 		if (compare > 0) {
-			System.out.println("Line 1 is greater than Line 2");
+			System.out.println("line1 is greater than line");
 		} else if (compare < 0) {
-			System.out.println("Line 1 is less than Line 2");
+			System.out.println("line1 is less than line2");
 		} else {
-			System.out.println("Line 1 is equal to Line 2");
+			System.out.println("line1 is equal to line2");
 		}
 	}
 
+	public static void main(String[] args) {
+		// function calling
+		compare();
+		equal();
+
+	}
 }
